248122dad8c01b3ceadd9b02aa3c1f38c7332a6e
Sara Bohannon <your.email@example.com>
2025-02-06 17:47:38 -0600

    passedTestTwo

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -18,6 +18,15 @@ public class EmployerController {
     @Autowired
     private EmployerRepository employerRepository;
 
+
+    //this get mapping was added 2/5
+    @GetMapping("")
+    public String index(Model model) {
+        Iterable<Employer> employers = employerRepository.findAll();
+        model.addAttribute("employers", employers);
+        return "employers/index";
+    }
+
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
         model.addAttribute(new Employer());
@@ -32,13 +41,15 @@ public class EmployerController {
             return "employers/add";
         }
 
+        employerRepository.save(newEmployer);
+
         return "redirect:";
     }
 
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional <Employer> optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
---
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -0,0 +1,63 @@
+package org.launchcode.techjobs.persistent.controllers;
+
+import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("")
+
+public class SkillController {
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        Iterable<Skill> skills = skillRepository.findAll();
+        model.addAttribute("skills", skills);
+        return "skills/index";
+    }
+
+    @GetMapping("add")
+    public String displayAddSkillForm(Model model) {
+        model.addAttribute(new Skill());
+        return "skills/add";
+    }
+
+    @PostMapping("add")
+    public String processAddSkillForm(@ModelAttribute @Valid Skill newSkill,
+                                         Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "skills/add";
+        }
+
+        skillRepository.save(newSkill);
+
+        return "redirect:";
+    }
+
+//is it skills or skill???*****************
+    @GetMapping("view/{skillId}")
+    public String displayViewSkill(Model model, @PathVariable int skillId) {
+
+        Optional<Skill> optSkill = skillRepository.findById(skillId);
+        if (optSkill.isPresent()) {
+            Skill skill = optSkill.get();
+            model.addAttribute("skill", skill);
+            return "skills/view";
+        } else {
+            return "redirect:../";
+        }
+
+    }
+}
